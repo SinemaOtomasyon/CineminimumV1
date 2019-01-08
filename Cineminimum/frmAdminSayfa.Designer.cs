da6572694571be562d0318e4d16a58cc9801dd72
Mrcaann <omurcankaba@gmail.com>
2019-01-08 16:21:50 +0300

    omur0801/1621

--- Cineminimum/frmAdminSayfa.Designer.cs
+++ Cineminimum/frmAdminSayfa.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmAdminSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(709, 450);
+            this.Name = "frmAdminSayfa";
             this.Text = "frmAdminSayfa";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
