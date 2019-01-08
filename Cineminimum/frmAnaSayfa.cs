2222317c9b8406c780810ab6cc26e4083a261059
UmutBasak <umut.basak58@gmail.com>
2019-01-08 15:40:25 +0300

    ilk

---
+++ Cineminimum/frmAnaSayfa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cineminimum
+{
+    public partial class frmAnaSayfa : Form
+    {
+        public frmAnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAnaSayfa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
